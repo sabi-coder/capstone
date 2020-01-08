7d42b83fd19c0c01d0c2f2926464aaf4ec049a1d
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-07 18:53:15 -0600

    :construction: Add component folders

---
+++ components/Header.js
@@ -0,0 +1,4 @@
+`<header>
+  <h1>Information Security Career Paths</h1>
+</header>
+`;
---
+++ components/Nav.js
@@ -0,0 +1,8 @@
+`<nav>
+  <ul class="flex justify-space-around">
+    <li class="button"><a href="#">Home</a></li>
+    <li class="button"><a href="./jobs/">Jobs</a></li>
+    <li class="button"><a href="./references/">References</a></li>
+    <li class="button"><a href="./contact-page/">Contact Page</a></li>
+  </ul>
+</nav>`;
