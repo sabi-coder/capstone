4cf7e03beb63ea610a0cd0720108ed78f68d2028
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-12 17:54:20 -0600

    Add JS files to each view

---
+++ store/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as Jobs } from "./Jobs";
+export { default as References } from "./References";
+export { default as Contact-Page } from "./Contact-Page";
+export { default as Links } from "./Links";
