19e46bd8ba0374cad8d4c6979d371189ffd6eefb
Nichole Herrington <nikkivp2003@gmail.com>
2020-01-15 18:04:04 -0600

    :construction: Add HTML code into view folders

---
+++ components/views/References.js
@@ -0,0 +1,14 @@
+export default st => `
+<div>
+<h2>Certifications</h2>
+<img src="../images/certifications.png" />
+<img src="../images/sans-roadmap.png" />
+<h3>The following certs are considered the top recommended:</h3>
+<ul class="text-align-center">
+  <li>Security+ (Sec+)</li>
+  <li>PenTest+</li>
+  <li>Cybersecurity Analyst+ (CySa+)</li>
+  <li>Advanced Security Practitioner (CASP+)</li>
+</ul>
+</div>
+`;
---
+++ components/views/index.js
@@ -0,0 +1,8 @@
+/**
+ * Export whatever comes out as the default from Header.js as 'Header.'
+ * All of this will be exported out as an ES Module with the given keys.
+ */
+export { default as Jobs } from "./Jobs";
+export { default as References } from "./References";
+export { default as Contact-Page } from "./Contact-Page";
+export { default as Home } from "./Home";
